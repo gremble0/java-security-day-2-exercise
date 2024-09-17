a83aa512f3fb69d71ef7e57097503ff43ad704c5
gremble0 <hermanstornes@gmail.com>
2024-09-17 14:47:14 +0200

    Mindless copy paste

--- src/main/java/com/booleanuk/api/repository/RoleRepository.java
+++ src/main/java/com/booleanuk/api/repository/RoleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-  Optional<Role> findByName(Role name);
+  Optional<Role> findByName(Role.Type name);
 }
--- src/main/java/com/booleanuk/library/security/SecurityConfiguration.java
+++ src/main/java/com/booleanuk/library/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.booleanuk.library.config;
+package com.booleanuk.library.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
