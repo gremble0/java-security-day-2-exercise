a594be31f458486d19f16088413c61d7fc743275
gremble0 <hermanstornes@gmail.com>
2024-09-17 09:04:27 +0200

    Add role class

---
+++ src/main/java/com/booleanuk/api/Role.java
@@ -0,0 +1,28 @@
+package com.booleanuk.api;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+  public enum Type {
+    USER,
+    ADMIN,
+    MODERATOR,
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Enumerated(EnumType.STRING)
+  private Type type;
+
+  public Role(Type type) {
+    this.type = type;
+  }
+}
---
+++ src/main/java/com/booleanuk/library/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.booleanuk.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(requests -> requests
+            .requestMatchers("/games", "/games/*", "/developers", "/developers/*", "/users", "/users/*").authenticated()
+            .requestMatchers("/login").permitAll()
+        )
+        .formLogin(form -> form.loginPage("/login").permitAll())
+        .logout(LogoutConfigurer::permitAll)
+        .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new InMemoryUserDetailsManager(User.withUsername("user")
+        .password("password")
+        .roles("ADMIN")
+        .build());
+  }
+}
